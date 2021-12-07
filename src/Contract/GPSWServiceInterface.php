514cb331013e950ca0e1c44e64a5370d37924aa0
Amin56413 <Ellie.guo@goldenf.co>
2021-12-07 18:53:59 +0800

    update coding style

--- src/Contract/GPSWServiceInterface.php
+++ src/Contract/GPSWServiceInterface.php
@@ -9,42 +9,42 @@ interface GPSWServiceInterface {
     /**
      * 遊戲啟動
      *
-     * @param string $op_code
+     * @param string $opCode
      * @param string $account
-     * @param string $game_code
+     * @param string $gameCode
      * @param string $language
-     * @param string $vendor_code
-     * @param string|null $back_url
+     * @param string $vendorCode
+     * @param string|null $backUrl
      * @return mixed
      */
-    function gameLaunch(string $op_code, string $account, string $game_code, string $language, string $vendor_code, string $back_url = null);
+    function gameLaunch(string $opCode, string $account, string $gameCode, string $language, string $vendorCode, string $backUrl = null);
 
     /**
      * 遊戲詳情
-     * @param string $op_code
+     * @param string $opCode
      * @param string $account
-     * @param string $bet_id
-     * @param string $vendor_code
+     * @param string $betId
+     * @param string $vendorCode
      * @return mixed
      */
-    function gameDetail(string $op_code, string $account, string $bet_id, string $vendor_code);
+    function gameDetail(string $opCode, string $account, string $betId, string $vendorCode);
 
     /**
      * 抓取遊戲紀錄
      *
-     * @param string $op_code
-     * @param integer $past_minutes
-     * @param string $cache_key
+     * @param string $opCode
+     * @param int $pastMinutes
+     * @param string $cacheKey
      * @return mixed
      */
-    function betLogGrabber(string $op_code, int $past_minutes, string $cache_key);
+    function betLogGrabber(string $opCode, int $pastMinutes, string $cacheKey);
 
     /**
      * 全營商 抓取遊戲紀錄
      *
-     * @param integer $past_minutes
+     * @param int $pastMinutes
      * @return mixed
      */
-    function betLogGrabberAll(int $past_minutes);
+    function betLogGrabberAll(int $pastMinutes);
 }
 
